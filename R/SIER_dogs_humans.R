8d707e1a3b91b43d8d42d947bed503bf4ac83720
Matteo Ciciani <matteo.ciciani@gmail.com>
2018-12-05 16:45:52 +0100

    SEIR dogs + humans

---
+++ R/SIER_dogs_humans.R
@@ -0,0 +1,105 @@
+library(deSolve)
+library(ggplot2)
+
+SEIR <- function(t, state, pars) {
+  with(as.list(c(state, pars)), {
+    
+    dS_d <- A + lambda * R_d + sigma * (1 - gamma) * E_d - beta * S_d * I_d - (m + k) * S_d
+    dE_d <- beta * S_d * I_d - sigma * (1 - gamma) * E_d - sigma * gamma * E_d - (m + k) * E_d
+    dI_d <- sigma * gamma * E_d - (m + mu) * I_d
+    dR_d <- k * (S_d + E_d) - (m + lambda) * R_d
+    
+    dS_h <- B + lambda_h * R_h + sigma_h * (1 - gamma_h) * E_h - beta_h * S_h * I_d - m_h  * S_h
+    dE_h <- beta_h * S_h * I_d - sigma_h * (1 - gamma_h) * E_h - sigma_h * gamma_h * E_h - (m_h + k_h) * E_h
+    dI_h <- sigma_h * gamma_h * E_h - (m_h + mu_h) * I_h
+    dR_h <- k_h * E_h - (m_h + lambda_h) * R_h
+    
+    return(list(c(dS_d, dE_d, dI_d, dR_d, dS_h, dE_h, dI_h, dR_h)))
+  })
+}
+
+pars <- c(
+  A = 3 * 10^(6), # month^(-1)
+  lambda = 1, # month^(-1)
+  sigma = 6, # monrt^(-1)
+  gamma = 0.4, # same
+  m = 0.08, # same
+  mu = 1, # same
+  k = 0.09, # same
+  beta = 1.58 * 10^(-7),
+  B = 1.54 * 10^7,
+  gamma_h = 0.4,
+  lambda_h = 1,
+  sigma_h = 6,
+  m_h = 0.0066,
+  beta_h = 2.29 * 10^(-12),
+  k_h = 0.54,
+  mu_h = 1
+) / 12
+
+# pars <- c(
+#   A = 2.34 * 10^5, # month^(-1)
+#   lambda = 1/6, # month^(-1)
+#   sigma = 1/1.045, # monrt^(-1)
+#   gamma = 0.49, # same
+#   m = 0.0064, # same
+#   mu = 1, # same
+#   k = 0.09, # same
+#   beta = 
+# )
+
+init <- c(S_d = 3.5 * 10^7, E_d = 2 * 10^5, I_d = 1 * 10^5, R_d = 2 * 10^5,
+  S_h = 1.29 * 10^9, E_h = 250, I_h = 89, R_h = 2 * 10^5)
+#init <- c(S_d = 3.5 * 10^2, E_d = 0, I_d = 1, R_d = 0)
+times <- seq(1, 50*12, by = 1)
+SEIR_out <- ode(init, times, SEIR, pars)
+
+myTheme <- theme(axis.text=element_text(size=20),
+  axis.title=element_text(size=25),
+  axis.title.y = element_text(margin = margin(r=20)),
+  axis.title.x = element_text(margin = margin(t=20)),
+  axis.ticks = element_line(size = .7),
+  axis.ticks.length = unit(.3, "cm"),
+  panel.background = element_rect(fill="white"),
+  panel.grid.major = element_line(colour = "grey90"),
+  panel.grid.minor = element_line(colour = "grey90"),
+  axis.line = element_line(color = "black", size = .7),
+  plot.title = element_text(hjust = 0.5, size = 25))
+
+# Dogs
+
+ggplot(data = as.data.frame(SEIR_out)) +
+  geom_line(mapping = aes(time, S_d), color = "blue") +
+  myTheme
+
+ggplot(data = as.data.frame(SEIR_out)) +
+  geom_line(mapping = aes(time, E_d), color = "orange") + myTheme
+
+ggplot(data = as.data.frame(SEIR_out)) +
+  geom_line(mapping = aes(time, I_d), color = "red") + myTheme
+
+ggplot(data = as.data.frame(SEIR_out)) +
+  geom_line(mapping = aes(time, R_d), color = "green") + myTheme
+
+# Humans
+
+ggplot(data = as.data.frame(SEIR_out)) +
+  geom_line(mapping = aes(time, S_h), color = "blue") +
+  myTheme
+
+ggplot(data = as.data.frame(SEIR_out)) +
+  geom_line(mapping = aes(time, E_h), color = "orange") + myTheme
+
+ggplot(data = as.data.frame(SEIR_out)) +
+  geom_line(mapping = aes(time, I_h), color = "red") + myTheme
+
+ggplot(data = as.data.frame(SEIR_out)) +
+  geom_line(mapping = aes(time, R_h), color = "green") + myTheme
+
+############################
+
+ggplot(data = as.data.frame(SEIR_out)) +
+  geom_line(mapping = aes(time, S_d), color = "blue") +
+  geom_line(mapping = aes(time, E_d), color = "orange") +
+  geom_line(mapping = aes(time, I_d), color = "red") + 
+  geom_line(mapping = aes(time, R_d), color = "green") + myTheme
--- R/seir-ode.r
+++ R/seir-ode.r
@@ -1,46 +1,47 @@
-rm(list=ls())
+rm(list = ls())
 library(deSolve)
 SEIR <- function(t, x, parms) {
     with(as.list(c(parms, x)), {
-        N <- S+E+I+R
-        dS <- - b*S*I/N  #susceptible
-        dE <- b*S*I/N - k*E #exposed
-        dI <- k*E - g*I  #infectious
-        dR <- g*I
+        N <- S + E + I + R
+        dS <- -b * S * I / N #susceptible
+        dE <- b * S * I / N - k * E #exposed
+        dI <- k * E - g * I  #infectious
+        dR <- g * I
         res <- c(dS, dE, dI, dR)
         list(res)
     })
 }
 
 ## Parameters
-transmission.rate=0.25
-duration.of.infection=10
-recovery.rate=1/duration.of.infection
-duration.of.NON.infection=6
-infection.rate=1/duration.of.NON.infection
-parms  <- c(b = transmission.rate, g = recovery.rate, k = infection.rate)
+transmission_rate <- 0.25
+duration_of_infection <- 10
+recovery_rate <- 1 / duration_of_infection
+duration_of_NON_infection <- 6
+infection_rate <- 1/duration_of_NON_infection
+parms <- c(b = transmission_rate, g = recovery_rate, k = infection_rate)
 
 ## vector of timesteps
-ndays=365
+ndays <- 365
 times  <- seq(0, ndays, length = 1001)
 
 ## initial conditions
-pop.size=100000
-S0=pop.size-1
-I0=1
-R0=0
-E0=0
+pop_size <- 100000
+S0 <- pop_size-1
+I0 <- 1
+R0 <- 0
+E0 <- 0
 ## Start values for steady state
-xstart <- c(S = S0, E=E0, I= I0, R = R0)
+xstart <- c(S = S0, E = E0, I = I0, R = R0)
 
 ## Solving
-out <-  lsoda(xstart, times, SIR, parms) 
+out <- lsoda(xstart, times, SEIR, parms) 
 
 ## Translate the output into a data.frame
 out2 <- as.data.frame(out)
 
 ## Plotting
-plot(out2$time, out2$S, col='blue', ylim=c(0, max(out[,-1])), type='l')
-lines(out2$time, out2$I, col='red')
-lines(out2$time, out2$R, col='orange')
-lines(out2$time, out2$E, col="green")
+plot(out2$time, out2$S, col = 'blue', ylim = c(0, max(out[, -1])), type = 'l')
+lines(out2$time, out2$I, col = 'red')
+lines(out2$time, out2$R, col = 'orange')
+lines(out2$time, out2$E, col = "green")
+
